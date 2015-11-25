a17111d9867d500294e2a36f594d8b954b942851
Tristan Crossley <trcrossley@gmail.com>
2015-11-25 17:32:26 +0000

    CH15 Refactored

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -6,17 +6,35 @@ class DockingStation
     @bikes = []
   end
 
+  def bikes
+    @bikes
+  end
+
   def dock(bikes)
-    fail 'Capacity is full' if @bikes.count >= 20
+    fail 'Capacity is full' if full?
     @bikes << bikes
   end
 
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
-  def bikes
-    @bikes
+  private
+
+  def full?
+    if @bikes.count >= 20
+      return true
+    else
+      return false
+    end
+  end
+
+  def empty?
+    if @bikes.count == 0
+      return true
+    else
+      return false
+    end
   end
 end
